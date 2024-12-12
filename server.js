feec2e9b079b7c3eed4cb45e01cfafb9a7e3946f
Yi-Ping Jiang <apple80177@gmail.com>
2024-12-12 22:16:08 +0800

    tmp

--- server.js
+++ server.js
@@ -1,11 +1,3 @@
-/*
- * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @Date: 2024-12-12 19:09:52
- * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2024-12-12 22:02:41
- * @FilePath: /vercel-api/server.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 const jsonServer = require('json-server')
 const clone = require('clone')
 const data = require('./db.json')
